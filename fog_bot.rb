4e33140259edebaf62d282678d8909d5b49d1a26
Danish Khan <danishkhan@danishkhan.org>
2010-12-14 14:21:09 -0800

    first fog_bot commit

---
+++ fog_bot.rb
@@ -0,0 +1,44 @@
+require 'cinch'
+require 'cinch-memo'
+
+Cinch::Plugins::Memo::Base.configure do |c|
+  c.store   = :redis          # data store
+  c.host    = 'localhost'     # your host
+  c.port    = '6379'          # your port
+end
+
+class Welcome
+  include Cinch::Plugin
+
+  listen_to :join
+  
+  # def listen(m)
+  #   unless m.user.nick == bot.nick
+  # end
+
+  def listen(m)
+    m.reply "Welcome to the fog irc channel! I'm the fog-bot. If you need any help just type !help and I will assist you."
+  end
+end
+
+class Help
+  include Cinch::Plugin
+
+  match "help"
+
+  def execute(m)
+    m.reply "!memo [nick] [message]: will save a memo for the user and fog-bot will let them know when they join the channel again or the user can type !memo? to view any messages left for them if they were idle"
+  end
+end
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server           = "irc.freenode.net"
+    c.nick             = "fog-bot"
+    c.channels         = ["#camanji"]
+    c.plugins.plugins  = [Cinch::Plugins::Memo::Base, Help, Welcome]
+  end
+
+end
+
+bot.start
